43ab249c2ad1fec52fbfa3ed68467bc9493d4933
Sachin-005 <sachinsenthilvel2005@gmail.com>
2025-06-03 12:18:05 +0530

    model

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,10 +1,24 @@
+import Movie from "../models/movie.model.js";
+
 export const MovieIndex = (req,res) =>{
     res.send("Get all movies");
 }
 
-export const MovieCreate = (req,res) =>{
+export const MovieCreate = async (req,res) =>{
     console.log(req.body)
-   return res.json(req.body)
+
+    // validate your data always
+  const newMovie =   new Movie({
+        title: req.body.title,
+        desc: req.body.desc
+    });
+  try{  
+  const movie = await newMovie.save();
+  return res.status(201).json(movie);
+  }
+  catch(error){
+    return res.status(400);
+  }
 }
 
 export const MovieUpdate = (req,res)=>{
